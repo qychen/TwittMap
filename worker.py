1b733f793cd91e79cf3abc6246664a5a09704bd2
Yi <YixingChen@dyn-160-39-223-107.dyn.columbia.edu>
2016-04-08 20:26:48 -0400

    worker Test

--- worker.py
+++ worker.py
@@ -1,5 +1,9 @@
 import json
 import boto3
+import urllib2
+import urllib
+
+#API KEY: d8d93e94d6a8f768cf2fb1c14cf24c7f3812f121
 
 # Get the service resource
 sqs = boto3.resource('sqs')
@@ -7,7 +11,38 @@ sqs = boto3.resource('sqs')
 # Get the queue
 queue = sqs.get_queue_by_name(QueueName='tweets')
 
+while True:
 # Process messages by printing out body and optional author name
-message = queue.receive_messages()
-print message.body
-message.delete()
+    message = queue.receive_messages()
+    print message.body
+    
+    data = {}
+    data['apikey'] = 'd8d93e94d6a8f768cf2fb1c14cf24c7f3812f121'
+    data['text'] = message.body
+    data['outputMode'] = 'json'
+    
+    url = 'http://gateway-a.watsonplatform.net/calls/text/TextGetTextSentiment'
+    post_data = urllib.urlencode(data)
+     
+    req = urllib2.urlopen(url, post_data)
+    
+    content = req.read()
+    print content
+    
+    decode = json.loads(content)
+    sentiment = str(decode['docSentiment']['type'])
+    print sentiment
+    
+    
+    #encode
+    Ansdata = {}
+    Ansdata['message'] = message.body
+    Ansdata['sentiment'] = sentiment
+    
+    Ans = json.dumps(Ansdata)
+    url = 'http://a3a175ba.ngrok.io/sns'
+    post_data = urllib.urlencode(Ans)
+    
+    req = urllib2.urlopen(url, post_data)
+    
+    message.delete()
